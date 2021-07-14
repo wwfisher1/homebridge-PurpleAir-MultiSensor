7abcf6056f50f1eec3ceeff2251ff1ddf13ff551
Bill Fisher <wwfisher@comcast.net>
2021-07-14 08:32:53 -0700

    timestamp 20210701 17:54

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ function PurpleAirAccessory(log, config) {
         this.includePM10 = config.includePM10 || false;
 
         this.log.info("PurpleAir is working");
+        this.log.info("statsKey is ", this.statsKey
 
   this.localIP = config.localIP;
   this.adjustTempF = config.adjustTempF;
